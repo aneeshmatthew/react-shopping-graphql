addafb207a7fbf2808a82f7dc1678a9121f09553
Aneesh Mathew <aneeshmathew@adobe.com>
2026-02-26 23:36:45 -0800

    add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Base path must match the GitHub repo name for GitHub Pages to serve assets correctly
+  base: '/react-shopping-graphql/',
   plugins: [react()],
   optimizeDeps: {
     include: ['@apollo/client', '@apollo/client/react'],
